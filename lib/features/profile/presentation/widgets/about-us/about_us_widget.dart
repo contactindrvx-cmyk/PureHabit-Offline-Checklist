931e4f0d860cbebd33ca494b0b8398b8942d618c
contactindrvx-cmyk <contact.indrvx@gmail.com>
2026-04-27 00:11:15 +0500

    Implement URL launching for social media buttons
    Added a function to launch URLs and updated social media buttons to use direct links.

--- lib/features/profile/presentation/widgets/about-us/about_us_widget.dart
+++ lib/features/profile/presentation/widgets/about-us/about_us_widget.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../../../../../config/locale/app_localization_helper.dart';
 import '../../../../../core/utils/app_colors.dart';
-import '../../../../../core/utils/app_localization_strings.dart';
 import '../../../../../core/utils/app_text_styles.dart';
 import '../../../../../core/widgets/buttons/button_widget.dart';
 import '../../../../../core/widgets/buttons/icon_text_button_widget.dart';
@@ -13,6 +11,15 @@ class AboutUsWidget extends StatelessWidget {
   final Function() onBackPressed;
 
   const AboutUsWidget({super.key, required this.onBackPressed});
+
+  // لنک اوپن کرنے کا فنکشن
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      debugPrint('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,40 +35,38 @@ class AboutUsWidget extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
+          // پہلی لائن: انسٹاگرام اور ٹک ٹاک
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               IconTextButton(
                 height: 40,
                 width: 150,
-                icon: LineAwesomeIcons.linkedin_in,
-                text: AppLocalizationHelper.translate(
-                    context, AppLocalizationKeys.linkedinName),
+                icon: LineAwesomeIcons.instagram,
+                text: 'Instagram',
                 onPressed: () {
-                  launch(AppLocalizationHelper.translate(
-                      context, AppLocalizationKeys.linkedinLink));
+                  _launchURL('https://instagram.com/aigrowthbox');
                 },
                 textStyle: AppTextStyles.profileIconTextButton,
                 backgroundColor: AppColors.secondary,
-                iconColor: AppColors.linkedin,
+                iconColor: Colors.pinkAccent, // انسٹاگرام کا کلر
               ),
               IconTextButton(
                 height: 40,
                 width: 150,
-                icon: LineAwesomeIcons.github,
-                text: AppLocalizationHelper.translate(
-                    context, AppLocalizationKeys.githubName),
+                icon: LineAwesomeIcons.tiktok,
+                text: 'TikTok',
                 onPressed: () {
-                  launch(AppLocalizationHelper.translate(
-                      context, AppLocalizationKeys.githubLink));
+                  _launchURL('https://www.tiktok.com/@aigrowthbox');
                 },
                 textStyle: AppTextStyles.profileIconTextButton,
                 backgroundColor: AppColors.secondary,
-                iconColor: AppColors.github,
+                iconColor: Colors.white, // ٹک ٹاک کا کلر
               ),
             ],
           ),
           const SizedBox(height: 20),
+          // دوسری لائن: واٹس ایپ چینل اور فیس بک
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: Row(
@@ -71,11 +76,9 @@ class AboutUsWidget extends StatelessWidget {
                   height: 40,
                   width: 150,
                   icon: LineAwesomeIcons.what_s_app,
-                  text: AppLocalizationHelper.translate(
-                      context, AppLocalizationKeys.whatsappName),
+                  text: 'WhatsApp',
                   onPressed: () {
-                    launch(AppLocalizationHelper.translate(
-                        context, AppLocalizationKeys.whatsappLink));
+                    _launchURL('https://whatsapp.com/channel/0029VbDBHXwBFLgZqG7PFf1C');
                   },
                   textStyle: AppTextStyles.profileIconTextButton,
                   backgroundColor: AppColors.secondary,
@@ -85,11 +88,9 @@ class AboutUsWidget extends StatelessWidget {
                   height: 40,
                   width: 150,
                   icon: LineAwesomeIcons.facebook_f,
-                  text: AppLocalizationHelper.translate(
-                      context, AppLocalizationKeys.facebookName),
+                  text: 'Facebook',
                   onPressed: () {
-                    launch(AppLocalizationHelper.translate(
-                        context, AppLocalizationKeys.facebookLink));
+                    _launchURL('https://facebook.com/aigrowthbox');
                   },
                   textStyle: AppTextStyles.profileIconTextButton,
                   backgroundColor: AppColors.secondary,
@@ -99,21 +100,21 @@ class AboutUsWidget extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
+          // تیسری لائن: آپ کا ای میل
           IconTextButton(
             height: 40,
             width: 150,
             icon: Icons.email_outlined,
-            text: AppLocalizationHelper.translate(
-                context, AppLocalizationKeys.mailName),
+            text: 'Email Us',
             onPressed: () {
-              launch(AppLocalizationHelper.translate(
-                  context, AppLocalizationKeys.mailLink));
+              _launchURL('mailto:contact@aigrowthbox.com');
             },
             textStyle: AppTextStyles.profileIconTextButton,
             backgroundColor: AppColors.secondary,
             iconColor: AppColors.outlook,
           ),
           const SizedBox(height: 50),
+          // بیک کا بٹن
           ButtonWidget(
             onPress: onBackPressed,
             backgroundColor: Colors.transparent,
@@ -123,8 +124,7 @@ class AboutUsWidget extends StatelessWidget {
             borderColor: AppColors.border,
             borderWidth: 0.7,
             child: Text(
-              AppLocalizationHelper.translate(
-                  context, AppLocalizationKeys.back),
+              'Back',
               textAlign: TextAlign.center,
               style: AppTextStyles.profileBackButtonText,
             ),
