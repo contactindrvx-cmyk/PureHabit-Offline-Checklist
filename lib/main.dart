57c13a38ac1252f993106baa8bb52afd5aa3cce5
contactindrvx-cmyk <contact.indrvx@gmail.com>
2026-04-27 01:11:29 +0500

    Add Google Mobile Ads initialization
    Initialize Google Mobile Ads SDK in the main function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart'; // ایڈموب کا پیکج
 import 'package:habit_it/app.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // گوگل ایڈموب کو سٹارٹ کرنے کا مین سوئچ
+  await MobileAds.instance.initialize();
+  
   await di.init();
   runApp(const HabitItApp());
 }
